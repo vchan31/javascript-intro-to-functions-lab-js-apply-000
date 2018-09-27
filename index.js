f6de5ba6cef5a704c247c3fa3ce63194253c541b
sensible-proc-7557 <vchan313@gmail.com>
2018-09-27 23:18:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string = 'hello'
+  if string === 'hello'
   return "I can't hear you!"
-  
+  if string === 'HELLO'
   return
 }
 
